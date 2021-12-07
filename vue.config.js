9089df69cbbf732ffce8fdbaec8b1d30b2647280
Zhong <weiaws@amazon.com>
2021-12-07 17:53:09 +0800

    initial refactor from vue cli

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  devServer: {
+    host: "localhost",
+  },
+  pluginOptions: {
+    quasar: {
+      importStrategy: "kebab",
+      rtlSupport: false,
+    },
+  },
+  transpileDependencies: ["quasar"],
+};
